a7b8bf74a6cff5b41e83656a4c2464868569df5a
Zia Ur Rehman <zia8397@gmail.com>
2019-02-22 03:21:49 +0500

    Initial express server

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:'mongodb://Zia123:Zia123@headsup-shard-00-00-vpu0v.gcp.mongodb.net:27017,headsup-shard-00-01-vpu0v.gcp.mongodb.net:27017,headsup-shard-00-02-vpu0v.gcp.mongodb.net:27017/test?ssl=true&replicaSet=HeadsUp-shard-0&authSource=admin&retryWrites=true'
+};
